e8b91810d4120a9405f704db5eb0535a4f4767db
aley-nay <38936568+aley-nay@users.noreply.github.com>
2022-12-27 01:10:53 +0300

    word counter in C

---
+++ word-counter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    char cumle[100];
+    printf("bir cumle giriniz:");
+    gets(cumle);
+    printf("%s",cumle);//cümleyi yazdır
+
+    int i=0;
+ 
+    while(cumle[i]!='\0'){    
+	    i++;
+    }
+    
+printf("%d adet kelime var",i);
+}
